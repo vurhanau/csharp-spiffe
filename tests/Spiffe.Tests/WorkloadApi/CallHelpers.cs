ee392ece054c7aec157a3e815e7ffaf4a04e303d
Alexander Vurganov <alex.vurganov@gmail.com>
2024-01-28 00:35:36 +0000

    wip

--- tests/Spiffe.Tests/WorkloadApi/CallHelpers.cs
+++ tests/Spiffe.Tests/WorkloadApi/CallHelpers.cs
@@ -17,12 +17,29 @@ internal class TestAsyncStreamReader<T> : IAsyncStreamReader<T>
         Task.Run(enumerator.MoveNext);
 }
 
+internal class TestErrorAsyncStreamReader<T> : IAsyncStreamReader<T>
+{
+    public T Current => throw new Exception("Oops!");
+
+    public Task<bool> MoveNext(CancellationToken cancellationToken) => throw new Exception("Oops!");
+}
+
 internal static class CallHelpers
 {
     public static AsyncServerStreamingCall<TResponse> CreateAsyncServerStreamingCall<TResponse>(params TResponse[] response)
     {
         return new AsyncServerStreamingCall<TResponse>(
             new TestAsyncStreamReader<TResponse>(response),
+            Task.FromResult(new Metadata()),
+            () => Status.DefaultSuccess,
+            () => new Metadata(),
+            () => { });
+    }
+
+    public static AsyncServerStreamingCall<TResponse> CreateAsyncServerStreamingErrorCall<TResponse>()
+    {
+        return new AsyncServerStreamingCall<TResponse>(
+            new TestErrorAsyncStreamReader<TResponse>(),
             Task.FromResult(new Metadata()),
             () => Status.DefaultSuccess,
             () => new Metadata(),
--- tests/Spiffe.Tests/WorkloadApi/TestWorkloadApiClient.cs
+++ tests/Spiffe.Tests/WorkloadApi/TestWorkloadApiClient.cs
@@ -32,36 +32,72 @@ public class TestWorkloadApiClient
         VerifyX509BundleSet(b, cert.RawData);
     }
 
-    // [Fact]
-    // public async Task TestWatchX509Bundles()
-    // {
-    //     using X509Certificate2 first = CertLoader.FirstFromPemFile("TestData/good-leaf-only.pem");
-    //     using X509Certificate2 second = CertLoader.FirstFromPemFile("TestData/good-leaf-and-intermediate.pem");
-    //     static X509BundlesResponse Resp(X509Certificate2 cert)
-    //     {
-    //         var r = new X509BundlesResponse();
-    //         r.Bundles.Add(TrustDomain.Name, ByteString.CopyFrom(cert.RawData));
-    //         return r;
-    //     }
+    [Fact]
+    public async Task TestWatchX509Bundles()
+    {
+        using X509Certificate2 first = CertLoader.FirstFromPemFile("TestData/good-leaf-only.pem");
+        using X509Certificate2 second = CertLoader.FirstFromPemFile("TestData/good-leaf-and-intermediate.pem");
+        static X509BundlesResponse Resp(X509Certificate2 cert)
+        {
+            var r = new X509BundlesResponse();
+            r.Bundles.Add(TrustDomain.Name, ByteString.CopyFrom(cert.RawData));
+            return r;
+        }
 
-    //     var mockGrpcClient = new Mock<SpiffeWorkloadAPIClient>();
-    //     mockGrpcClient.Setup(c => c.FetchX509Bundles(It.IsAny<X509BundlesRequest>(), It.IsAny<CallOptions>()))
-    //                   .Returns(CallHelpers.CreateAsyncServerStreamingCall(
-    //                                             Resp(first),
-    //                                             Resp(second)));
+        var mockGrpcClient = new Mock<SpiffeWorkloadAPIClient>();
+        mockGrpcClient.Setup(c => c.FetchX509Bundles(It.IsAny<X509BundlesRequest>(), It.IsAny<CallOptions>()))
+                      .Returns(CallHelpers.CreateAsyncServerStreamingCall(
+                                                Resp(first),
+                                                Resp(second)));
 
-    //     var c = new WorkloadApiClient(mockGrpcClient.Object, _ => { }, NullLogger.Instance);
-    //     var watcher = new Mock<IWatcher<X509BundleSet>>();
-    //     watcher
-    //         .Setup(w => w.OnUpdate(It.IsAny<X509BundleSet>()))
-    //         .Verifiable();
+        var c = new WorkloadApiClient(mockGrpcClient.Object, _ => { }, NullLogger.Instance);
 
-    //     using CancellationTokenSource cts = new();
-    //     await c.WatchX509BundlesAsync(watcher.Object, cts.Token);
+        List<X509BundleSet> receivedBundles = [];
+        List<Exception> exceptions = [];
+        var watcher = new Watcher<X509BundleSet>(receivedBundles.Add, exceptions.Add);
 
-    //     watcher.Verify(w => w.OnUpdate(It.IsAny<X509BundleSet>()), Times.Exactly(2));
-    //     cts.Cancel();
-    // }
+        using CancellationTokenSource cts = new();
+        cts.CancelAfter(100);
+        await c.WatchX509BundlesAsync(watcher, cts.Token);
+
+        receivedBundles.Should().HaveCount(2);
+
+        X509BundleSet b0 = receivedBundles[0];
+        b0.Bundles.Should().ContainSingle();
+        b0.Bundles.Should().ContainKey(TrustDomain);
+        b0.Bundles[TrustDomain].X509Authorities.Should().ContainSingle();
+        b0.Bundles[TrustDomain].X509Authorities[0].RawData.Should().Equal(first.RawData);
+
+        X509BundleSet b1 = receivedBundles[1];
+        b1.Bundles.Should().ContainSingle();
+        b1.Bundles.Should().ContainKey(TrustDomain);
+        b1.Bundles[TrustDomain].X509Authorities.Should().ContainSingle();
+        b1.Bundles[TrustDomain].X509Authorities[0].RawData.Should().Equal(second.RawData);
+
+        exceptions.Should().BeEmpty();
+    }
+
+    // TODO: fix
+    [Fact]
+    public async Task TestWatchX509BundlesError()
+    {
+        var mockGrpcClient = new Mock<SpiffeWorkloadAPIClient>();
+        mockGrpcClient.Setup(c => c.FetchX509Bundles(It.IsAny<X509BundlesRequest>(), It.IsAny<CallOptions>()))
+                      .Returns(CallHelpers.CreateAsyncServerStreamingErrorCall<X509BundlesResponse>());
+
+        var c = new WorkloadApiClient(mockGrpcClient.Object, _ => { }, NullLogger.Instance);
+
+        List<X509BundleSet> receivedBundles = [];
+        List<Exception> exceptions = [];
+        var watcher = new Watcher<X509BundleSet>(receivedBundles.Add, exceptions.Add);
+
+        using CancellationTokenSource cts = new();
+        cts.CancelAfter(100);
+        await c.WatchX509BundlesAsync(watcher, cts.Token);
+
+        exceptions.Should().NotBeEmpty();
+        receivedBundles.Should().BeEmpty();
+    }
 
     [Fact]
     public async Task TestFetchX509Svids()
