0261d5eae5ce579d98c37e19f647df8872602908
Alexander Vurganov <alex.vurganov@gmail.com>
2024-02-05 23:51:24 +0000

    wip

---
+++ samples/Grpc/Mtls/MtlsGrpcClient/Program.cs
@@ -0,0 +1,8 @@
+﻿using Grpc.Net.Client;
+using GrpcCommon;
+using static GrpcCommon.Greeter;
+
+using GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7000");
+GreeterClient client = new(channel);
+HelloReply reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine($"Reply: {reply.Message}");
