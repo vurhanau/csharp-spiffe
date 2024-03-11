9d14924741b0b5ae5648db3857df9e0f283f1881
Alexander Vurganov <vurhanau@users.noreply.github.com>
2024-03-11 00:02:09 +0000

    Bundle source JWT support (#64)

--- tests/Spiffe.Tests/WorkloadApi/TestBundleSource.cs
+++ tests/Spiffe.Tests/WorkloadApi/TestBundleSource.cs
@@ -1,10 +1,13 @@
 ﻿using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 using FluentAssertions;
 using Google.Protobuf;
 using Grpc.Core;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.IdentityModel.Tokens;
 using Moq;
+using Spiffe.Bundle.Jwt;
 using Spiffe.Bundle.X509;
 using Spiffe.Id;
 using Spiffe.Tests.Helper;
@@ -16,17 +19,18 @@ namespace Spiffe.Tests.WorkloadApi;
 public class TestBundleSource
 {
     [Fact(Timeout = 10_000)]
-    public async Task TestGetX509Bundle()
+    public async Task TestGetBundles()
     {
-        SpiffeId spiffeId = SpiffeId.FromString("spiffe://example.org/workload");
+        TrustDomain td = TrustDomain.FromString("spiffe://example.org");
+        SpiffeId spiffeId = SpiffeId.FromPath(td, "/workload");
         using X509Certificate2 bundleCert = Certificates.FirstFromPemFile("TestData/X509/good-leaf-only.pem");
         using X509Certificate2 svidCert = X509Certificate2.CreateFromPemFile(
             "TestData/X509/good-leaf-only.pem",
             "TestData/X509/key-pkcs8-rsa.pem");
         byte[] svidKey = svidCert.GetRSAPrivateKey()!.ExportPkcs8PrivateKey();
         string hint = "internal";
-        X509SVIDResponse resp = new();
-        resp.Svids.Add(new X509SVID()
+        X509SVIDResponse x509Resp = new();
+        x509Resp.Svids.Add(new X509SVID()
         {
             SpiffeId = spiffeId.Id,
             Bundle = ByteString.CopyFrom(bundleCert.RawData),
@@ -35,16 +39,35 @@ public class TestBundleSource
             Hint = hint,
         });
 
+        JWTBundlesResponse jwtResp = new();
+        CA ca = CA.Create(td);
+        JwtBundle b = ca.JwtBundle();
+        byte[] bundleBytes = JwtBundles.Serialize(b);
+        jwtResp.Bundles.Add(td.Name, ByteString.CopyFrom(bundleBytes));
+
         Mock<SpiffeWorkloadAPIClient> mockGrpcClient = new();
         mockGrpcClient.Setup(c => c.FetchX509SVID(It.IsAny<X509SVIDRequest>(), It.IsAny<CallOptions>()))
-                      .Returns(CallHelpers.Stream(resp));
+                      .Returns(CallHelpers.Stream(x509Resp));
+        mockGrpcClient.Setup(c => c.FetchJWTBundles(It.IsAny<JWTBundlesRequest>(), It.IsAny<CallOptions>()))
+                      .Returns(CallHelpers.Stream(jwtResp));
+
         WorkloadApiClient c = new(mockGrpcClient.Object, _ => { }, NullLogger.Instance);
         using BundleSource s = await BundleSource.CreateAsync(c);
 
-        X509Bundle bundle = s.GetX509Bundle(spiffeId.TrustDomain);
-        bundle.TrustDomain.Should().Be(spiffeId.TrustDomain);
-        bundle.X509Authorities.Should().ContainSingle();
-        bundle.X509Authorities[0].RawData.Should().Equal(bundleCert.RawData);
+        X509Bundle x509Bundle = s.GetX509Bundle(td);
+        x509Bundle.TrustDomain.Should().Be(td);
+        x509Bundle.X509Authorities.Should().ContainSingle();
+        x509Bundle.X509Authorities[0].RawData.Should().Equal(bundleCert.RawData);
+
+        JwtBundle jwtBundle = s.GetJwtBundle(td);
+        jwtBundle.TrustDomain.Should().Be(td);
+        jwtBundle.JwtAuthorities.Should().ContainSingle();
+        jwtBundle.JwtAuthorities.Keys.Should().Equal(b.JwtAuthorities.Keys);
+        JsonWebKey k1 = jwtBundle.JwtAuthorities.First().Value;
+        JsonWebKey k2 = b.JwtAuthorities.First().Value;
+        string json1 = JsonSerializer.Serialize(k1);
+        string json2 = JsonSerializer.Serialize(k2);
+        json1.Should().Be(json2);
     }
 
     [Fact(Timeout = 10_000)]
@@ -81,17 +104,80 @@ public class TestBundleSource
         await Assert.ThrowsAsync<OperationCanceledException>(() => BundleSource.CreateAsync(c, timeoutMillis: 500));
     }
 
+    [Fact(Timeout = 10_000)]
+    public async Task TestCreateJwtTimedOut()
+    {
+        TrustDomain td = TrustDomain.FromString("spiffe://example.org");
+        SpiffeId spiffeId = SpiffeId.FromPath(td, "/workload");
+        using X509Certificate2 bundleCert = Certificates.FirstFromPemFile("TestData/X509/good-leaf-only.pem");
+        using X509Certificate2 svidCert = X509Certificate2.CreateFromPemFile(
+            "TestData/X509/good-leaf-only.pem",
+            "TestData/X509/key-pkcs8-rsa.pem");
+        byte[] svidKey = svidCert.GetRSAPrivateKey()!.ExportPkcs8PrivateKey();
+        string hint = "internal";
+        X509SVIDResponse x509Resp = new();
+        x509Resp.Svids.Add(new X509SVID()
+        {
+            SpiffeId = spiffeId.Id,
+            Bundle = ByteString.CopyFrom(bundleCert.RawData),
+            X509Svid = ByteString.CopyFrom(svidCert.RawData),
+            X509SvidKey = ByteString.CopyFrom(svidKey),
+            Hint = hint,
+        });
+
+        JWTBundlesResponse jwtResp = new();
+        TimeSpan respDelay = TimeSpan.FromHours(1);
+
+        Mock<SpiffeWorkloadAPIClient> mockGrpcClient = new();
+        mockGrpcClient.Setup(c => c.FetchX509SVID(It.IsAny<X509SVIDRequest>(), It.IsAny<CallOptions>()))
+                      .Returns(CallHelpers.Stream(x509Resp));
+        mockGrpcClient.Setup(c => c.FetchJWTBundles(It.IsAny<JWTBundlesRequest>(), It.IsAny<CallOptions>()))
+                      .Returns(CallHelpers.Stream(respDelay, jwtResp));
+
+        WorkloadApiClient c = new(mockGrpcClient.Object, _ => { }, NullLogger.Instance);
+
+        await Assert.ThrowsAsync<OperationCanceledException>(() => BundleSource.CreateAsync(c, timeoutMillis: 500));
+    }
+
+    [Fact(Timeout = 10_000)]
+    public async Task TestCreateX509TimedOut()
+    {
+        TrustDomain td = TrustDomain.FromString("spiffe://example.org");
+        X509SVIDResponse x509Resp = new();
+        TimeSpan respDelay = TimeSpan.FromHours(1);
+
+        JWTBundlesResponse jwtResp = new();
+        CA ca = CA.Create(td);
+        JwtBundle b = ca.JwtBundle();
+        byte[] bundleBytes = JwtBundles.Serialize(b);
+        jwtResp.Bundles.Add(td.Name, ByteString.CopyFrom(bundleBytes));
+
+        Mock<SpiffeWorkloadAPIClient> mockGrpcClient = new();
+        mockGrpcClient.Setup(c => c.FetchX509SVID(It.IsAny<X509SVIDRequest>(), It.IsAny<CallOptions>()))
+                      .Returns(CallHelpers.Stream(respDelay, x509Resp));
+        mockGrpcClient.Setup(c => c.FetchJWTBundles(It.IsAny<JWTBundlesRequest>(), It.IsAny<CallOptions>()))
+                      .Returns(CallHelpers.Stream(respDelay, jwtResp));
+
+        WorkloadApiClient c = new(mockGrpcClient.Object, _ => { }, NullLogger.Instance);
+
+        await Assert.ThrowsAsync<OperationCanceledException>(() => BundleSource.CreateAsync(c, timeoutMillis: 500));
+    }
+
     [Fact]
     public void TestFailWhenInvalidState()
     {
+        TrustDomain td = TrustDomain.FromString("spiffe://example.org");
         BundleSource s = new();
-        Action f1 = () => s.GetX509Bundle(TrustDomain.FromString("spiffe://example.org"));
-
+        Action f1 = () => s.GetX509Bundle(td);
+        Action f2 = () => s.GetJwtBundle(td);
         f1.Should().Throw<InvalidOperationException>();
+        f2.Should().Throw<InvalidOperationException>();
 
         s.SetX509Context(new([], new([])));
+        s.SetJwtBundles(new([]));
         s.Dispose();
 
         f1.Should().Throw<ObjectDisposedException>();
+        f2.Should().Throw<ObjectDisposedException>();
     }
 }
