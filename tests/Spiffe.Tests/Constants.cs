1af791d075ca788b75f556c5357520906463866d
Alexander Vurganov <vurhanau@users.noreply.github.com>
2025-04-16 00:45:00 +0100

    Bump version to 0.0.1 (#310)
    * Bump version to 0.0.1
    
    * Increase timeout for windows tests

--- tests/Spiffe.Tests/Constants.cs
+++ tests/Spiffe.Tests/Constants.cs
@@ -2,9 +2,15 @@ namespace Spiffe.Tests;
 
 internal static class Constants
 {
+#if OS_WINDOWS
+    public const int TestTimeoutMillis = 30_000;
+
+    public const int IntegrationTestTimeoutMillis = 60_000;
+#else
     public const int TestTimeoutMillis = 10_000;
 
     public const int IntegrationTestTimeoutMillis = 30_000;
+#endif
 
     public const int IntegrationTestRetriesMax = 5;
 
